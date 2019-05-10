d22293b5002c68fb5f39a6b9a59bbd974e7c37a2
Alexis Josephides <ajosephides@gmail.com>
2019-05-10 17:14:25 +0100

    added a default choice and refactored filtering

--- directory.rb
+++ directory.rb
@@ -1,21 +1,35 @@
 
 def input_students
+  exit = false
   puts "Please enter the names of the students"
-  puts "To finish, just hit the return twice"
+  puts "To finish, type 'exit' "
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
+  # continue to add names til exit is typed in
+  while !exit do
+    puts "Please enter the student name"
+    name = user_input
+    puts "Please enter their cohort"
+    cohort = user_input
+    if name.upcase == 'EXIT' || cohort.upcase == 'EXIT'
+      exit = true      
+    else
     # add the student to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort.to_sym}
     puts "Now we have #{students.count} students"
-    # get another name from the user
-    name = gets.chomp
   end
+end
   # return array of students
-  students
+  return students
+end
+
+def user_input
+  user_input = gets.chomp
+  if user_input.empty?
+    return "Unknown"
+  else
+    return user_input
+  end
 end
 
 def print_header
@@ -23,42 +37,36 @@ def print_header
   puts "-------------"
 end
 
-def print(students, filter)
-  if filter[:letter_filter].empty? && filter[:number_filter].empty?
-    students.each_with_index { 
-      |student, index | puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    }
-  elsif !filter[:letter_filter].empty? && filter[:number_filter].empty?
-    students.each_with_index { 
-      |student, index| if student[:name][0].upcase == filter[:letter_filter].upcase then 
-      puts("#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)") 
-    end 
-    }
-  elsif filter[:letter_filter].empty? && !filter[:number_filter].empty?
-    students.each_with_index {
-      |student, index| if student[:name].length <= filter[:number_filter].to_i then 
-        puts("#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)") 
-      end 
-    }
-    elsif !filter[:letter_filter].empty? && !filter[:number_filter].empty?
-    students.each_with_index {
-        |student, index| if (student[:name][0].upcase == filter[:letter_filter].upcase && 
-          student[:name].length <= filter[:number_filter].to_i) then
-          puts("#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)") 
-        end 
-    }
-  end 
+def print(students)
+  students.each_with_index { 
+    |student, index | puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  }
 end
 
-def student_filter
-  filter = {}
-  puts "Please enter first letter of students name that you wish to print"
-  letter_filter = gets.chomp
-  filter[:letter_filter] = letter_filter
-  puts "Please enter maximum number of characters for a name"
-  number_filter = gets.chomp
-  filter[:number_filter] = number_filter
-  return filter
+def filter_choice
+puts "Please select an option to filter by"
+puts "0. No filter"
+puts "1. Filter by first letter of name"
+puts "2. Filter by number of characters in name"
+puts "9. Exit"
+return gets.chomp
+end
+
+def student_filter(students, filter_choice)
+  case filter_choice
+  when "0"
+    return students
+  when "1"
+    puts "Please enter first letter of students name that you wish to print"
+    letter_filter = gets.chomp
+    return students.select { |student| student[:name][0].upcase == letter_filter.upcase}
+  when "2"
+    puts "Please enter maximum number of characters for a name"
+    number_filter = gets.chomp
+    return students.select { |student| student[:name].length <= number_filter.to_i}
+  when "9"
+    exit
+  end
 end
 
 def print_footer(names)
@@ -68,5 +76,5 @@ end
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students, student_filter)
+print(student_filter(students, filter_choice))
 print_footer(students)
