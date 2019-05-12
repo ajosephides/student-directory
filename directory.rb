3a2c9e4e3a8d8971e2debc2e3f1c4fd20c9438e2
Alexis Josephides <ajosephides@gmail.com>
2019-05-12 16:24:58 +0100

    refactored interactive menu and program to use global students array

--- directory.rb
+++ directory.rb
@@ -1,34 +1,33 @@
-
-def interactive_menu
-  students = []
+@students = []
+def print_menu
   loop do
   puts "1. Input the students"
   puts "2. Filter the students"
   puts "3. Show the students"
   puts "9. Exit"
-  selection = gets.chomp
+  process_choice(gets.chomp)
+  end
+end
+
+def process_choice(selection)
   case selection
   when "1"
-    students = input_students
+    input_students
   when "2"
-    students = student_filter(students, filter_choice)
+    print_filtered(student_filter(filter_choice))
   when "3"
-    print_header
-    print(students)
-    print_footer(students)
+    show_students
   when "9"
     exit
   else
     puts "Please enter one of the choices again"
   end
 end
-end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish hit return twice when entering a name"
-  students = []
- return add_students(students, user_input)
+ return add_students(user_input)
 end
 
 def user_input
@@ -40,16 +39,15 @@ def user_input
   end
 end
 
-def add_students(students, name)
+def add_students(name)
   while name != "Unknown" do
     puts "Please enter their cohort months"
     cohort = user_input
-    students << {name: name, cohort: cohort.to_sym}
-    puts "Now we have #{students.count} students\n"
+    @students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{@students.count} students\n"
     puts "Please enter the next student's name"
     name = user_input
   end
-  return students
 end
 
 def filter_choice
@@ -62,30 +60,30 @@ puts "9. Exit"
 return gets.chomp
 end
 
-def student_filter(students, filter_choice)
+def student_filter(filter_choice)
   case filter_choice
   when "0"
-    return students
+    puts "No filter applied"
   when "1"
     puts "Please enter first letter of students name that you wish to print"
     letter_filter = gets.chomp
-    return students.select { |student| student[:name][0].upcase == letter_filter.upcase}
+    return @students.select { |student| student[:name][0].upcase == letter_filter.upcase}
   when "2"
     puts "Please enter maximum number of characters for a name"
     number_filter = gets.chomp
-    return students.select { |student| student[:name].length <= number_filter.to_i}
+    return @students.select { |student| student[:name].length <= number_filter.to_i}
   when "3"
-    puts "Please choose from the following cohorts #{group_options(students).join(", ")}"
+    puts "Please choose from the following cohorts #{group_options.join(", ")}"
     cohort_filter = gets.chomp
-    return students.select { |student| student[:cohort].to_s.upcase == cohort_filter.upcase}
+    return @students.select { |student| student[:cohort].to_s.upcase == cohort_filter.upcase}
   when "9"
     exit
   end
 end
 
-def group_options(students)
+def group_options
 options = []
-students.each { |student| options << student[:cohort].to_s.capitalize}
+@students.each { |student| options << student[:cohort].to_s.capitalize}
 return options.uniq
 end
 
@@ -94,19 +92,26 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each_with_index { 
+def print
+  @students.each_with_index { 
     |student, index | puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great student#{if names.count > 1 then "s" end}"
+def print_filtered(filtered_students)
+  filtered_students.each_with_index { 
+    |student, index | puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+  }
 end
 
-#nothing happens until we call the methods
-#students = input_students
-#print_header
-#print(student_filter(students, filter_choice))
-#print_footer(students)
-interactive_menu
+def print_footer
+  puts "Overall, we have #{@students.count} great student#{if @students.count > 1 then "s" end}"
+end
+
+def show_students
+  print_header
+  print
+  print_footer
+end
+
+print_menu
