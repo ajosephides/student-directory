8f3affc53a2891d4146c372f4a32ad064309feca
Alexis Josephides <ajosephides@gmail.com>
2019-05-14 14:37:30 +0100

    removed unneeded code since adding print menu class

--- lib/directory.rb
+++ lib/directory.rb
@@ -8,14 +8,6 @@ require_relative './menu.rb'
 
 def print_menu
   loop do
-  #puts "\n--------------------------------"
-  #puts "1. Input the students manually"
-  #puts "2. Load students from a file"
-  #puts "3. Filter the students"
-  #puts "4. Show the students"
-  #puts "5. Save the students"
-  #puts "9. Exit"
-  #puts "--------------------------------\n"
   @menu.print_main
   process_choice(@user.menu_selection)
   end
@@ -62,12 +54,6 @@ def add_student(name, cohort)
 end
 
 def filter_choice
-#puts "Please select an option to filter by"
-#puts "0. No filter"
-#puts "1. Filter by first letter of name"
-#puts "2. Filter by number of characters in name"
-#puts "3. Filter by cohort"
-#puts "9. Exit"
 @menu.print_filter
 return @user.filter_selection
 end
