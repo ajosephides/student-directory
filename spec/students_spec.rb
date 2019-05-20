50efbca21340c3e265479b37e9445b6e6b833d7e
Alexis Josephides <ajosephides@gmail.com>
2019-05-20 16:21:54 +0100

    added a new test with helper function to test adding student

--- spec/students_spec.rb
+++ spec/students_spec.rb
@@ -8,12 +8,25 @@ describe 'Students' do
     Students.new
   }
 
+  let(:adding_students){
+    students = Students.new
+    student1 = Student.new("name","cohort")
+    student2 = Student.new("name","cohort")
+    students.add_student(student1)
+    students.add_student(student2)
+    students.number
+  }
+
   it 'creates a new empty students array' do
     expect(Students.new.all_students.count).to eq(0) 
   end
   
   it 'can create a students' do
     expect(students).to be_a_kind_of(Students)
+  end
+
+  it 'should have added 2 students' do
+    expect(adding_students).to eq(2)
   end
 
   
