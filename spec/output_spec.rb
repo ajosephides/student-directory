4a24b6c9ae6abd53d155722842ab06e0ea59b8b3
Alexis Josephides <ajosephides@gmail.com>
2019-05-20 15:29:15 +0100

    added another output spec and some tests for user inputs

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -16,8 +16,16 @@ describe 'Output' do
     expect{ @output.print_header }.to output("The students of Villains Academy\n-------------\n").to_stdout
   end
 
-  it 'will output student count when 2 students are in @students' do
-    expect { @output.print_footer(@students) }.to output("Overall, we have 2 great students\n").to_stdout
+  it 'will output student count of 2 when 2 students are in @students' do
+    expect{ @output.print_footer(@students) }.to output("Overall, we have 2 great students\n").to_stdout
+  end
+
+  it 'will return nil for some functions e.g save_file to be nil' do
+    expect(@output.save_file).to be_nil
+    expect(@output.input_students).to be_nil
+    expect(@output.success_load).to be_nil
+    expect(@output.success_save).to be_nil
+    expect(@output.success_student_added(@students)).to be_nil
   end
 
 end
