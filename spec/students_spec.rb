a25df2a20b8c66bd2f26b791d66bf26b7cf47b82
Alexis Josephides <ajosephides@gmail.com>
2019-05-20 16:14:48 +0100

    another test for Students class

--- spec/students_spec.rb
+++ spec/students_spec.rb
@@ -4,16 +4,17 @@ require 'student'
 
 describe 'Students' do
 
+  let(:students) {
+    Students.new
+  }
+
   it 'creates a new empty students array' do
     expect(Students.new.all_students.count).to eq(0) 
   end
   
+  it 'can create a students' do
+    expect(students).to be_a_kind_of(Students)
+  end
+
   
-
-
-
-
-
-
-
 end
