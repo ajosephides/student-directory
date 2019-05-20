95213ffd9650509780617fe550c2e0471a7aa0bc
Alexis Josephides <ajosephides@gmail.com>
2019-05-20 16:25:19 +0100

    test to check creation of a student

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,6 +2,10 @@ require 'student'
 
 describe 'Student' do
 
+  let(:student_correct){
+    Student.new("name","cohort")
+  }
+
   it "should raise" do
     expect{Student.new}.to raise_error(ArgumentError)
   end
@@ -9,5 +13,9 @@ describe 'Student' do
   it "should raise" do
     expect{Student.new("name", "cohort", "something else")}.to raise_error(ArgumentError)
   end 
+
+  it "should create a Student type object" do
+    expect(student_correct).to be_a_kind_of(Student)
+  end
 
 end
