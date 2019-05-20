aaed2c5be3e2b060b9ac4d0ed1a39b4384ef328a
Alexis Josephides <ajosephides@gmail.com>
2019-05-20 16:05:49 +0100

    added new file and tests for Students class

---
+++ spec/students_spec.rb
@@ -0,0 +1,19 @@
+require 'output'
+require 'students'
+require 'student'
+
+describe 'Students' do
+
+  it 'creates a new empty students array' do
+    expect(Students.new.all_students.count).to eq(0) 
+  end
+  
+  
+
+
+
+
+
+
+
+end
